f6406dd8f83e2d7b4973201fded3e3bb7eca595f
Rodrigo Fontes <rodrigo_fnts@hotmail.com>
2023-01-07 22:30:50 -0300

    Layout da aplicação

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,3 +1,13 @@
+import styles from './header.module.scss';
+
 export default function Header() {
-  // TODO
+  return (
+    <header className={styles.mainHeader}>
+      <div className={styles.mainHeaderContainer}>
+        <a href="/">
+          <img src="/logo.svg" alt="logo" />
+        </a>
+      </div>
+    </header>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import { GetStaticProps } from 'next';
+import Header from '../components/Header';
+import { FiCalendar, FiUser } from 'react-icons/fi';
 
 import { getPrismicClient } from '../services/prismic';
 
@@ -24,9 +26,107 @@ interface HomeProps {
   postsPagination: PostPagination;
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home() {
+  return (
+    <>
+      <Header />
+
+      <main className={commonStyles.container}>
+        <div className={styles.posts}>
+          <article className={styles.post}>
+            <h2>Como utilizar Hooks</h2>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+
+            <footer>
+              <span>
+                <FiCalendar />
+                15 Mar 2021
+              </span>
+              <span>
+                <FiUser />
+                Joseph Oliveira
+              </span>
+            </footer>
+          </article>
+
+          <article className={styles.post}>
+            <h2>Criando um app CRA do zero</h2>
+            <p>
+              Tudo sobre como criar a sua primeira aplicação utilizando Create
+              React App.
+            </p>
+
+            <footer>
+              <span>
+                <FiCalendar />
+                19 Abr 2021
+              </span>
+              <span>
+                <FiUser />
+                Danilo Vieira
+              </span>
+            </footer>
+          </article>
+
+          <article className={styles.post}>
+            <h2>Como utilizar Hooks</h2>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+
+            <footer>
+              <span>
+                <FiCalendar />
+                15 Mar 2021
+              </span>
+              <span>
+                <FiUser />
+                Joseph Oliveira
+              </span>
+            </footer>
+          </article>
+
+          <article className={styles.post}>
+            <h2>Criando um app CRA do zero</h2>
+            <p>
+              Tudo sobre como criar a sua primeira aplicação utilizando Create
+              React App.
+            </p>
+
+            <footer>
+              <span>
+                <FiCalendar />
+                19 Abr 2021
+              </span>
+              <span>
+                <FiUser />
+                Danilo Vieira
+              </span>
+            </footer>
+          </article>
+
+          <article className={styles.post}>
+            <h2>Como utilizar Hooks</h2>
+            <p>Pensando em sincronização em vez de ciclos de vida.</p>
+
+            <footer>
+              <span>
+                <FiCalendar />
+                15 Mar 2021
+              </span>
+              <span>
+                <FiUser />
+                Joseph Oliveira
+              </span>
+            </footer>
+          </article>
+        </div>
+
+        <div className={styles.paginator}>
+          <button type="button">Carregar mais posts</button>
+        </div>
+      </main>
+    </>
+  );
+}
 
 // export const getStaticProps = async () => {
 //   // const prismic = getPrismicClient({});
